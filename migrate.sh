198faa9e2fbc459be4794d50b3770f9754a59ae3
Rivers Chen <riverscbc47@gmail.com>
2025-03-12 08:42:04 -0400

    Improve migrate script

--- migrate.sh
+++ migrate.sh
@@ -1,12 +1,19 @@
-export OLD_VERSION="v1.128.0"
-export NEW_VERSION="v1.129.0"
-wget "https://github.com/immich-app/immich/releases/download/$NEW_VERSION/docker-compose.yml"
-wget "https://github.com/immich-app/immich/releases/download/$NEW_VERSION/example.env"
-wget "https://github.com/immich-app/immich/releases/download/$NEW_VERSION/hwaccel.ml.yml"
-wget "https://github.com/immich-app/immich/releases/download/$NEW_VERSION/hwaccel.transcoding.yml"
-wget "https://github.com/immich-app/immich/releases/download/$NEW_VERSION/prometheus.yml"
+OLD_VERSION="$1"
+NEW_VERSION="$2"
+if [ -z $2 ]; then
+	echo "usage: ./migrate.sh v1.128.0 v1.129.0"
+	exit
+fi
+echo "$1 -> $2"
+
+wget -q "https://github.com/immich-app/immich/releases/download/$NEW_VERSION/docker-compose.yml"
 
 DB_IMAGE_TAG=$(yq e '.services.database.image' docker-compose.yml)
 sed -i "s|^Image=.*$|Image=${DB_IMAGE_TAG}|" immich-database.container
+REDIS_IMAGE_TAG=$(yq e '.services.redis.image' docker-compose.yml)
+sed -i "s|^Image=.*$|Image=${REDIS_IMAGE_TAG}|" immich-redis.container
+
 
 sed -i "s/${OLD_VERSION}/${NEW_VERSION}/" *.container *.image
+
+rm docker-compose.yml
