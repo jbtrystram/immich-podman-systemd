e6b7d236a0bcf1727160e2642ebded58978fcce3
Rivers Chen <riverscbc47@gmail.com>
2025-05-28 16:43:46 -0400

    Bump version to v1.134.0

--- migrate.sh
+++ migrate.sh
@@ -1,10 +1,9 @@
-OLD_VERSION="$1"
-NEW_VERSION="$2"
-if [ -z $2 ]; then
-	echo "usage: ./migrate.sh v1.128.0 v1.129.0"
+NEW_VERSION="$1"
+if [ -z $1 ]; then
+	echo "usage: ./migrate.sh v1.129.0"
 	exit
 fi
-echo "$1 -> $2"
+echo "migrate to $1"
 
 wget -q "https://github.com/immich-app/immich/releases/download/$NEW_VERSION/docker-compose.yml"
 
@@ -14,6 +13,6 @@ REDIS_IMAGE_TAG=$(yq e '.services.redis.image' docker-compose.yml)
 sed -i "s|^Image=.*$|Image=${REDIS_IMAGE_TAG}|" immich-redis.container
 
 
-sed -i "s/${OLD_VERSION}/${NEW_VERSION}/" *.container *.image
+sed -i "s/v[0-9]\.[0-9]\{3\}\.[0-9]/${NEW_VERSION}/" *.container *.image
 
 rm docker-compose.yml
